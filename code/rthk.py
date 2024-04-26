1749585a391c71e2eb68ba782a2d3660b31d38bc
raileo98 <164594063+raileo98@users.noreply.github.com>
2024-04-26 03:14:46 +0000

    Update rthk.py

--- code/rthk.py
+++ code/rthk.py
@@ -17,62 +17,74 @@ proxies = {'http':'socks5h://localhost:50000', 'https':'socks5h://localhost:5000
 session = niquests.Session(resolver="doh://9.9.9.9") # , happy_eyeballs=True)
 session.headers['Cache-Control'] = 'no-cache'
 session.headers['Pragma'] = 'no-cache'
-session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; rv:124.0) Gecko/20100101 Firefox/124.0'
+session.headers['User-Agent'] = secrets.choice(['Mozilla/5.0 (Windows NT 10.0; rv:124.0) Gecko/20100101 Firefox/124.0', 'Mozilla/5.0 (Windows NT 10.0; rv:125.0) Gecko/20100101 Firefox/125.0', 'Mozilla/5.0 (Windows NT 10.0; rv:123.0) Gecko/20100101 Firefox/123.0'])
 session.proxies.update(proxies)
 
 def parse_pub_date(date_str):
     date_str = date_str.replace('HKT', '+0800')
     date_obj = datetime.strptime(date_str, '%Y-%m-%d %z %H:%M')
     return date_obj.strftime('%a, %d %b %Y %H:%M:%S %z')
+    
+def get_item_pub_date(item):
+    pub_date = item.find('pubDate')
+    if pub_date:
+        return pub_date.text
+
+    published = item.find('published')
+    if published:
+        return published.text
+
+    return None
+
 
 categories_data = {
-    "hk_rthk_ch": {
-        "title": "rthk",
-        "url": "https://news.rthk.hk/rthk/ch/latest-news.htm"
+    'hk_rthk_ch': {
+        'title': 'rthk',
+        'url': 'https://news.rthk.hk/rthk/ch/latest-news.htm'
     },
-    "hk_rthk_local_ch": {
-        "title": "rthk - 本地",
-        "url": "https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=3&newsCount=60&dayShiftMode=1&archive_date="
+    'hk_rthk_local_ch': {
+        'title': 'rthk - 本地',
+        'url': 'https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=3&newsCount=60&dayShiftMode=1&archive_date='
     },
-    "hk_rthk_greaterChina_ch": {
-        "title": "rthk - 大中華",
-        "url": "https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=2&newsCount=60&dayShiftMode=1&archive_date="
+    'hk_rthk_greaterChina_ch': {
+        'title': 'rthk - 大中華',
+        'url': 'https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=2&newsCount=60&dayShiftMode=1&archive_date='
     },
-    "hk_rthk_world_ch": {
-        "title": "rthk - 國際",
-        "url": "https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=4&newsCount=60&dayShiftMode=1&archive_date="
+    'hk_rthk_world_ch': {
+        'title': 'rthk - 國際',
+        'url': 'https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=4&newsCount=60&dayShiftMode=1&archive_date='
     },
-    "hk_rthk_finance_ch": {
-        "title": "rthk - 財經",
-        "url": "https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=5&newsCount=60&dayShiftMode=1&archive_date="
+    'hk_rthk_finance_ch': {
+        'title': 'rthk - 財經',
+        'url': 'https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=5&newsCount=60&dayShiftMode=1&archive_date='
     },
-    "hk_rthk_sport_ch": {
-        "title": "rthk - 體育",
-        "url": "https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=6&newsCount=60&dayShiftMode=1&archive_date="
+    'hk_rthk_sport_ch': {
+        'title': 'rthk - 體育',
+        'url': 'https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=6&newsCount=60&dayShiftMode=1&archive_date='
     },
-    "hk_rthk_en": {
-        "title": "rthk - English",
-        "url": "https://news.rthk.hk/rthk/en/latest-news.htm"
+    'hk_rthk_en': {
+        'title': 'rthk - English',
+        'url': 'https://news.rthk.hk/rthk/en/latest-news.htm'
     },
-    "hk_rthk_local_en": {
-        "title": "rthk - Local",
-        "url": "https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=en-GB&cat=8&newsCount=60&dayShiftMode=1&archive_date="
+    'hk_rthk_local_en': {
+        'title': 'rthk - Local',
+        'url': 'https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=en-GB&cat=8&newsCount=60&dayShiftMode=1&archive_date='
     },
-    "hk_rthk_greaterChina_en": {
-        "title": "rthk - Greater China",
-        "url": "https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=en-GB&cat=9&newsCount=60&dayShiftMode=1&archive_date="
+    'hk_rthk_greaterChina_en': {
+        'title': 'rthk - Greater China',
+        'url': 'https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=en-GB&cat=9&newsCount=60&dayShiftMode=1&archive_date='
     },
-    "hk_rthk_world_en": {
-        "title": "rthk - World",
-        "url": "https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=en-GB&cat=10&newsCount=60&dayShiftMode=1&archive_date="
+    'hk_rthk_world_en': {
+        'title': 'rthk - World',
+        'url': 'https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=en-GB&cat=10&newsCount=60&dayShiftMode=1&archive_date='
     },
-    "hk_rthk_finance_en": {
-        "title": "rthk - Finance",
-        "url": "https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=en-GB&cat=12&newsCount=60&dayShiftMode=1&archive_date="
+    'hk_rthk_finance_en': {
+        'title': 'rthk - Finance',
+        'url': 'https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=en-GB&cat=12&newsCount=60&dayShiftMode=1&archive_date='
     },
-    "hk_rthk_sport_en": {
-        "title": "rthk - Sport",
-        "url": "https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=en-GB&cat=11&newsCount=60&dayShiftMode=1&archive_date="
+    'hk_rthk_sport_en': {
+        'title': 'rthk - Sport',
+        'url': 'https://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=en-GB&cat=11&newsCount=60&dayShiftMode=1&archive_date='
     }
 }
 
@@ -107,7 +119,7 @@ for category, url in urls_list:
     fg.link(href=url, rel='alternate')
     fg.language('zh-HK')
 
-    feedImg = f"https://images.weserv.nl/?n=-1&url={urllib.parse.quote_plus('https://external-content.duckduckgo.com/ip3/' + url + '.ico')}"
+    feedImg = f"https://images.weserv.nl/?n=-1&url={urllib.parse.quote_plus('https://external-content.duckduckgo.com/ip3/' + urllib.parse.urlparse(url).netloc + '.ico')}"
     fg.logo(feedImg)
 
     fg.copyright('© 香港電台 RTHK')
@@ -163,6 +175,17 @@ for category, url in urls_list:
     if soup_rss.find('url') is not None:
         soup_rss.find('url').string = CData(html.unescape(soup_rss.find('url').string))
     
+    # 排序 feed 項目
+    sorted_items = sorted(soup_rss.find_all('item'), key=lambda x: datetime.strptime(get_item_pub_date(x), '%a, %d %b %Y %H:%M:%S %z') if get_item_pub_date(x) else datetime.min, reverse=True)
+
+    # 移除原有的項目
+    for item in soup_rss.find_all('item'):
+        item.extract()
+
+    # 將排序後的項目重新加入 feed
+    for item in sorted_items:
+        soup_rss.channel.append(item)
+
     with open(rss_filename, 'w', encoding='utf-8') as file:
         file.write(soup_rss.prettify())
 
