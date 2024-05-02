60bfe79dd1c2a98a75f0c2b1e591f1b4f3fd1707
raileo98 <164594063+raileo98@users.noreply.github.com>
2024-05-02 05:22:53 +0000

    Update rthk.py

--- code/rthk.py
+++ code/rthk.py
@@ -23,7 +23,7 @@ session.headers['Cache-Control'] = 'no-cache'
 session.headers['Pragma'] = 'no-cache'
 userAgent = ['Mozilla/5.0 (Windows NT 10.0; rv:124.0) Gecko/20100101 Firefox/124.0', 'Mozilla/5.0 (Windows NT 10.0; rv:125.0) Gecko/20100101 Firefox/125.0', 'Mozilla/5.0 (Windows NT 10.0; rv:123.0) Gecko/20100101 Firefox/123.0']
 session.headers['User-Agent'] = secrets.choice(userAgent)
-# session.proxies.update(proxies)
+session.proxies.update(proxies)
 
 def parse_pub_date(date_str):
     date_str = date_str.replace('HKT', '+0800')
