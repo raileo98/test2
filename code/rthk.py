faa97e7aee7eae9cb6c207856e1b238d1e724489
raileo98 <164594063+raileo98@users.noreply.github.com>
2024-05-04 05:42:44 +0000

    Update rthk.py

--- code/rthk.py
+++ code/rthk.py
@@ -13,8 +13,8 @@ from asyncio import Semaphore
 
 # 設置代理和HTTP客戶端
 proxies = {'http': 'socks5h://localhost:50000', 'https': 'socks5h://localhost:50000'}
-session = niquests.AsyncSession(resolver="doh://mozilla.cloudflare-dns.com", retries=3, pool_connections=10, pool_maxsize=100)
-# session = niquests.AsyncSession(retries=1, pool_connections=30, pool_maxsize=100)
+# session = niquests.AsyncSession(resolver="doh://mozilla.cloudflare-dns.com", retries=3, pool_connections=10, pool_maxsize=100)
+session = niquests.AsyncSession(retries=3, pool_connections=10, pool_maxsize=100)
 session.headers['Cache-Control'] = 'no-cache'
 session.headers['Pragma'] = 'no-cache'
 userAgent = [
