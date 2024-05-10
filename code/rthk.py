8b148251828a8ca34508cdb8f99ad33233fc4944
raileo98 <164594063+raileo98@users.noreply.github.com>
2024-05-10 04:25:57 +0000

    Update rthk.py

--- code/rthk.py
+++ code/rthk.py
@@ -114,8 +114,8 @@ async def process_category(category, url):
     fg.link(href=categories_data[category]['url'], rel='alternate')
     fg.language('zh-HK')
 
-    # feedImg = f"https://images.weserv.nl/?n=-1&ll&output=webp&url={urllib.parse.quote_plus('https://external-content.duckduckgo.com/ip3/' + urllib.parse.urlparse(categories_data[category]['url']).netloc + '.ico')}"
-    feedImg = f"https://images.weserv.nl/?n=-1&output=webp&url={urllib.parse.quote_plus('https://favicone.com/' + urllib.parse.urlparse(categories_data[category]['url']).netloc)}"
+    # feedImg = f"http://localhost:8080/?n=-1&ll&output=webp&url={urllib.parse.quote_plus('https://external-content.duckduckgo.com/ip3/' + urllib.parse.urlparse(categories_data[category]['url']).netloc + '.ico')}"
+    feedImg = f"http://localhost:8080/?n=-1&output=webp&url={urllib.parse.quote_plus('https://favicone.com/' + urllib.parse.urlparse(categories_data[category]['url']).netloc)}"
     fg.logo(feedImg)
 
     fg.copyright('© 香港電台 RTHK')
@@ -189,7 +189,7 @@ async def process_article(fg, category, article):
     imgHtml = ''
     imgList = set()
     for image in images:
-        imgUrl = 'https://images.weserv.nl/?n=-1&output=webp&url=' + urllib.parse.quote_plus(image['src'])
+        imgUrl = 'http://localhost:8080/?n=-1&output=webp&url=' + urllib.parse.quote_plus(image['src'])
         print(f'{articleLink} - {articleTitle}: {imgUrl}')
         imgList.add(imgUrl)
         
@@ -258,7 +258,7 @@ async def process_article(fg, category, article):
             match = re.search(r"videoThumbnail\s*=\s*'(.*)'", script.text)
             if match:
                 video_thumbnail = match.group(1)
-                imgUrl = 'https://images.weserv.nl/?n=-1&output=webp&url=' + urllib.parse.quote_plus(video_thumbnail)
+                imgUrl = 'http://localhost:8080/?n=-1&output=webp&url=' + urllib.parse.quote_plus(video_thumbnail)
                 imgList.add(imgUrl)
                 
                 imgUrl = imgUrl.replace('_S_', '_L_').replace('_M_', '_L_')
