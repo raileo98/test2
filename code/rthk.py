f83c0b1798d328e9a273ff998686fc39defb932c
raileo98 <164594063+raileo98@users.noreply.github.com>
2024-05-12 04:52:38 +0000

    Update rthk.py

--- code/rthk.py
+++ code/rthk.py
@@ -15,7 +15,7 @@ import threading
 
 # 設置代理和HTTP客戶端
 proxies = {'http': 'socks5h://localhost:50000', 'https': 'socks5h://localhost:50000'}
-session = niquests.Session(resolver="doh://mozilla.cloudflare-dns.com/dns-query", pool_connections=10, pool_maxsize=1000, retries=3)
+session = niquests.Session(resolver="doh://mozilla.cloudflare-dns.com/dns-query", pool_connections=10, pool_maxsize=10000, retries=3)
 session.quic_cache_layer.add_domain('images.weserv.nl')
 session.quic_cache_layer.add_domain('mozilla.cloudflare-dns.com')
 session.headers['Cache-Control'] = 'no-cache'
