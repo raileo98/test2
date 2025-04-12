4c77311698c0bed93612268c502630b80d08cf32
raileo98 <164594063+raileo98@users.noreply.github.com>
2025-04-12 09:07:42 +0000

    Update rthk.py

--- code/rthk.py
+++ code/rthk.py
@@ -291,7 +291,12 @@ async def process_article(fg, category, article):
         imgList = set()
         images = article_soup.select('.items_content .imgPhotoAfterLoad')
         for image in images:
-            raw_img_url = 'https://i3.wp.com/' + image['src'].replace('http://', '').replace('https://', '')
+            raw_img_url = image['src']
+
+            if not urlparse(raw_img_url).query:
+                raw_img_url = raw_img_url.rstrip('?')
+                raw_img_url = 'https://i3.wp.com/' + raw_img_url.replace('http://', '').replace('https://', '')
+            
             raw_img_url = 'https://external-content.duckduckgo.com/iu/?u=' + urllib.parse.quote( raw_img_url, safe='' )
             raw_img_url = 'https://wsrv.nl/?n=-1&we&w=720&output=webp&trim=1&url=' + urllib.parse.quote( raw_img_url, safe='' )
             imgUrl = modify_image_url(raw_img_url, 99).replace('_S_', '_L_').replace('_M_', '_L_').replace('_L_', '_')
@@ -308,9 +313,15 @@ async def process_article(fg, category, article):
                     match = re.search(r"videoThumbnail\s{0,1000}=\s{0,1000}'(.*)'", script.text)
                     if match:
                         video_thumbnail = match.group(1)
-                        raw_img_url = 'https://i3.wp.com/' + video_thumbnail.replace('http://', '').replace('https://', '')
+                        raw_img_url = video_thumbnail
+            
+                        if not urlparse(raw_img_url).query:
+                            raw_img_url = raw_img_url.rstrip('?')
+                            raw_img_url = 'https://i3.wp.com/' + raw_img_url.replace('http://', '').replace('https://', '')
+                        
                         raw_img_url = 'https://external-content.duckduckgo.com/iu/?u=' + urllib.parse.quote( raw_img_url, safe='' )
                         raw_img_url = 'https://wsrv.nl/?n=-1&we&w=720&output=webp&trim=1&url=' + urllib.parse.quote( raw_img_url, safe='' )
+                            
                         imgUrl = modify_image_url(raw_img_url, 99).replace('_S_', '_L_').replace('_M_', '_L_').replace('_L_', '_')
                         imgList.add(imgUrl)
                         latest_imgUrl = await optimize_image_quality(imgUrl)
@@ -429,9 +440,13 @@ async def process_category(category, url):
     fg.language('zh-HK')
     # feedImg = 'https://wsrv.nl/?n=-1&url=https://i3.wp.com/news.rthk.hk/rthk/templates/st_tyneo/favicon_144x144.png'
     feedImg = 'https://news.rthk.hk/rthk/templates/st_tyneo/favicon_144x144.png'
-    feedImg = 'https://i3.wp.com/' + feedImg.replace('http://', '').replace('https://', '')
-    feedImg = 'https://external-content.duckduckgo.com/iu/?u=' + urllib.parse.quote( feedImg, safe='' )
-    feedImg = 'https://wsrv.nl/?n=-1&we&w=720&output=webp&trim=1&url=' + urllib.parse.quote( feedImg, safe='' )
+
+    if not urlparse(feedImg).query:
+        feedImg = feedImg.rstrip('?')
+        feedImg = 'https://i3.wp.com/' + feedImg.replace('http://', '').replace('https://', '')
+            
+    feedImg = 'https://external-content.duckduckgo.com/iu/?u=' + urllib.parse.quote( raw_img_url, safe='' )
+    feedImg = 'https://wsrv.nl/?n=-1&we&w=720&output=webp&trim=1&url=' + urllib.parse.quote( raw_img_url, safe='' )
     fg.logo(feedImg)
     fg.copyright('© 香港電台 RTHK')
     fg.webMaster('webmaster@rthk.hk')
